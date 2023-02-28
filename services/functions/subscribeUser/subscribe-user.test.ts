5d6446ba67158027ced953e6896bb60bc56f3820
ethan-stone <ethan.stone9352@gmail.com>
2023-02-28 15:32:26 -0500

    updated tests

--- services/functions/subscribeUser/subscribe-user.test.ts
+++ services/functions/subscribeUser/subscribe-user.test.ts
@@ -1,15 +1,58 @@
-import { InsertUserFn, User } from "@/db/user";
+import { GetUserByEmailFn, InsertUserFn, User } from "@/db/user";
 import { SendEmailFn } from "@/email/send-email";
 import { describe, expect, it, vi } from "vitest";
-import { subscribeUser } from "./subscribe-user";
+import { EmailInUseError, subscribeUser } from "./subscribe-user";
 
 describe("subscribe-user tests", () => {
+  it("should throw EmailInUseError if a user is found by email", async () => {
+    const user = {
+      id: "id",
+      email: "email",
+      name: "name",
+    } satisfies User;
+
+    const mockGetUserByEmail = vi
+      .fn<Parameters<GetUserByEmailFn>, ReturnType<GetUserByEmailFn>>()
+      .mockResolvedValueOnce(user);
+
+    const mockInsertUser = vi
+      .fn<Parameters<InsertUserFn>, ReturnType<InsertUserFn>>()
+      .mockResolvedValueOnce(user);
+
+    const mockSendEmail = vi
+      .fn<Parameters<SendEmailFn>, ReturnType<SendEmailFn>>()
+      .mockResolvedValueOnce();
+
+    try {
+      await subscribeUser(
+        {
+          email: user.email,
+          name: user.name,
+        },
+        {
+          getUserByEmail: mockGetUserByEmail,
+          insertUser: mockInsertUser,
+          sendEmail: mockSendEmail,
+        }
+      );
+    } catch (error) {
+      expect(error).toBeInstanceOf(EmailInUseError);
+      expect(mockGetUserByEmail).toHaveBeenCalledTimes(1);
+      expect(mockInsertUser).toHaveBeenCalledTimes(0);
+      expect(mockSendEmail).toHaveBeenCalledTimes(0);
+    }
+  });
+
   it("should return user if insert and send email succeed", async () => {
     const user = {
       id: "id",
       email: "email",
       name: "name",
     } satisfies User;
+
+    const mockGetUserByEmail = vi
+      .fn<Parameters<GetUserByEmailFn>, ReturnType<GetUserByEmailFn>>()
+      .mockResolvedValueOnce(null);
 
     const mockInsertUser = vi
       .fn<Parameters<InsertUserFn>, ReturnType<InsertUserFn>>()
@@ -25,11 +68,13 @@ describe("subscribe-user tests", () => {
         name: user.name,
       },
       {
+        getUserByEmail: mockGetUserByEmail,
         insertUser: mockInsertUser,
         sendEmail: mockSendEmail,
       }
     );
 
+    expect(mockGetUserByEmail).toHaveBeenCalledTimes(1);
     expect(mockInsertUser).toHaveBeenCalledTimes(1);
     expect(mockSendEmail).toHaveBeenCalledTimes(1);
     expect(res).toEqual(user);
